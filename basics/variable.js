351cfe5d5e3233b15a615c15b6a966d575b1c7af
Dipanshu Singh <ds23322seven@gmail.com>
2025-06-19 07:02:30 +0000

    some changes in variable

--- basics/variable.js
+++ basics/variable.js
@@ -1,7 +1,13 @@
-const userPassword="12345"
-let userName="Dipanshu Singh"
-var userWork="Mining"
+"use strict";
+// const userPassword="12345"
+// let userName="Dipanshu Singh"
+// var userWork="Web Developer"
 
-userName="Aayush Singh"
-console.log(userName)
+// userName="Aayush Singh"
+// console.log(userName)
 // node basics/variable.js
+// console.table([userPassword,userName,userWork])
+//We should use let instead of var because ofscope and functional sxcope issues
+let passKey;
+let jesKey=undefined;
+console.log("0"==0)
