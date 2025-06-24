cb7d28ca66bcdf2e01694224caa615c4b78842b6
Dipanshu Singh <ds23322seven@gmail.com>
2025-06-24 05:46:43 +0000

    learned for loops

---
+++ basics/iteration.js
@@ -0,0 +1,10 @@
+for (let i = 1; i < 10; i++) {
+    console.log(`The number is ${i}`);
+    
+    for (let j = 1; j < 10; j++) {
+        const number = j;
+        console.log(`${i} X ${j} = ${i*j}`);
+        
+    }
+} 
+// node basics/iteration.js
