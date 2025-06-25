65d6144d105e0b5762aab92b7f4299727e00744c
Dipanshu Singh <ds23322seven@gmail.com>
2025-06-25 05:21:52 +0000

    learning about maps in js

---
+++ basics/maps.js
@@ -0,0 +1,31 @@
+// ls=[12,6,7,56,21]
+// for (const list of ls) {
+//     console.log(list);
+// }
+
+
+// maps=new Map()
+// maps.set("Rs","Ruppes")
+// maps.set("Dl","Dollar")
+// maps.set("Yn","Yen")
+// maps.set("Gr","Germany")
+// for (const [key,values] of maps) {
+//     console.log(`${key} :- ${values}`)
+// }
+
+
+
+// const object1={
+//     city1:"Delhi",
+//     city2:"Mumbai",
+//     city3:"Prayagraj",
+//     city4:"Lucknow"
+// }
+// for (const obj in object1) {
+//     console.log(`${obj} stand for ${object1[obj]}`)
+// }
+
+l=[1,24,5,7,8]
+for (const key in l) {
+    console.log(l[key])
+}
